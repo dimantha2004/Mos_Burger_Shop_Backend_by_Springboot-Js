578611b1c40fc635b64b04a7f934fa35d22e2707
Dimantha Theekshana <dimanthatheekshana12345@gmail.com>
2025-03-19 20:35:55 +0530

    All done

--- Mos-Burger-Backend/src/main/java/edu/icet/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package edu.icet.service;
-
-
-import edu.icet.entity.User;
-
-public interface UserService {
-    void registerUser(User userDTO);
-    boolean authenticateUser(String username, String password);
-}
